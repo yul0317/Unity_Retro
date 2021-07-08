73a59c2fd46db7c90046a163310e59ca0efb8bf3
yul0317 <68114089+yul0317@users.noreply.github.com>
2021-07-08 18:01:06 +0900

    20210708_1800

--- Uni-Run/Assets/Scripts/GameManager.cs
+++ Uni-Run/Assets/Scripts/GameManager.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 
 // 게임 오버 상태를 표현하고, 게임 점수와 UI를 관리하는 게임 매니저
 // 씬에는 단 하나의 게임 매니저만 존재할 수 있다.
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
     public static GameManager instance; // 싱글톤을 할당할 전역 변수
 
     public bool isGameover = false; // 게임 오버 상태
@@ -14,7 +15,8 @@ public class GameManager : MonoBehaviour {
     private int score = 0; // 게임 점수
 
     // 게임 시작과 동시에 싱글톤을 구성
-    void Awake() {
+    void Awake()
+    {
         // 싱글톤 변수 instance가 비어있는가?
         if (instance == null)
         {
@@ -32,17 +34,34 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    void Update() {
+    void Update()
+    {
         // 게임 오버 상태에서 게임을 재시작할 수 있게 하는 처리
+        if (isGameover && Input.GetMouseButtonDown(0))
+        {
+            //게임오버 상태에서 마우스 왼쪽 버튼을 클릭하면 현재 씬 재시작
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     // 점수를 증가시키는 메서드
-    public void AddScore(int newScore) {
-        
+    public void AddScore(int newScore)
+    {
+        //게임 오버가 아니라면
+        if (!isGameover)
+        {
+            //점수를 증가
+            score += newScore;
+            scoreText.text = "Score : " + score;
+        }
     }
 
     // 플레이어 캐릭터가 사망시 게임 오버를 실행하는 메서드
-    public void OnPlayerDead() {
-        
+    public void OnPlayerDead()
+    {
+        //현재상태를 게임오버 상태로 변경
+        isGameover = true;
+        //게임오버 UI를 활성화
+        gameoverUI.SetActive(true);
     }
 }
---
+++ Zombie/Assets/Scripts/Enemy.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI; // AI, 내비게이션 시스템 관련 코드를 가져오기
+
+// 적 AI를 구현한다
+public class Enemy : LivingEntity
+{
+    public LayerMask whatIsTarget; // 추적 대상 레이어
+
+    private LivingEntity targetEntity; // 추적할 대상
+    private NavMeshAgent pathFinder; // 경로계산 AI 에이전트
+
+    public ParticleSystem hitEffect; // 피격시 재생할 파티클 효과
+    public AudioClip deathSound; // 사망시 재생할 소리
+    public AudioClip hitSound; // 피격시 재생할 소리
+
+    private Animator enemyAnimator; // 애니메이터 컴포넌트
+    private AudioSource enemyAudioPlayer; // 오디오 소스 컴포넌트
+    private Renderer enemyRenderer; // 렌더러 컴포넌트
+
+    public float damage = 20f; // 공격력
+    public float timeBetAttack = 0.5f; // 공격 간격
+    private float lastAttackTime; // 마지막 공격 시점
+
+    // 추적할 대상이 존재하는지 알려주는 프로퍼티
+    private bool hasTarget
+    {
+        get
+        {
+            // 추적할 대상이 존재하고, 대상이 사망하지 않았다면 true
+            if (targetEntity != null && !targetEntity.dead)
+            {
+                return true;
+            }
+
+            // 그렇지 않다면 false
+            return false;
+        }
+    }
+
+    private void Awake()
+    {
+        // 초기화
+        //게임 오브젝트로부터 사용할 컴포넌트 가져오기
+        pathFinder = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        enemyAudioPlayer = GetComponent<AudioSource>();
+
+        //렌더러 컴포넌트는 자식 게임 오브젝트에 있으므로
+        //GetComponentInChildren() 메서드 사용
+        enemyRenderer = GetComponentInChildren<Renderer>();
+    }
+
+    // 적 AI의 초기 스펙을 결정하는 셋업 메서드
+    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor)
+    {
+        //체력설정
+        startingHealth = newHealth;
+        health = newHealth;
+        //공격력 설정
+        damage = newDamage;
+        //내비메시 에이전트의 이동 속도 설정
+        pathFinder.speed = newSpeed;
+        //렌더러가 사용 중인 머티리얼의 컬러를 변경, 외형 색이 변함
+        enemyRenderer.material.color = skinColor;
+    }
+
+    private void Start()
+    {
+        // 게임 오브젝트 활성화와 동시에 AI의 추적 루틴 시작
+        StartCoroutine(UpdatePath());
+    }
+
+    private void Update()
+    {
+        // 추적 대상의 존재 여부에 따라 다른 애니메이션을 재생
+        enemyAnimator.SetBool("HasTarget", hasTarget);
+    }
+
+    // 주기적으로 추적할 대상의 위치를 찾아 경로를 갱신
+    private IEnumerator UpdatePath()
+    {
+        // 살아있는 동안 무한 루프
+        while (!dead)
+        {
+            if (hasTarget)
+            {
+                //추적대상 존재 : 경로를 갱신하고 AI 이동을 계속 진행
+                pathFinder.isStopped = false;
+                pathFinder.SetDestination(targetEntity.transform.position);
+            }
+            else
+            {
+                //추적 대상 없음 : AI 이동 중지
+                pathFinder.isStopped = true;
+
+                //20유닛의 반지름을 가진 가상의 구를 그렸을때 구와 겹치는 모든 콜라이더를 가져옴
+                //단, whatIsTarget 레이어를 가진 콜라이더만 가져오도록 필터링
+                Collider[] colliders = Physics.OverlapSphere(transform.position, 20f, whatIsTarget);
+
+                //모든 콜라이더를 순회하면서 살아 있는 LivingEntity 찾기
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    //콜라이더로부터 LivingEntity 컴포넌트 가져오기
+                    LivingEntity livingEntity = colliders[i].GetComponent<LivingEntity>();
+
+                    //LivingEntity 컴포넌트가 존재하며, 해당 LivingEntity가 살아 있다면
+                    if (livingEntity != null && !livingEntity.dead)
+                    {
+                        //추적 대상을 해당 LivingEntity로 설정
+                        targetEntity = livingEntity;
+
+                        //for 문 루프 즉시 정지
+                        break;
+                    }
+                }
+            }
+            // 0.25초 주기로 처리 반복
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    // 데미지를 입었을때 실행할 처리
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        //아직 사망하지 않은 경우에만 피격효과 재생
+        if (!dead)
+        {
+            //공격받은 지점과 방향으로 파티클 효과 재생
+            hitEffect.transform.position = hitPoint;
+            hitEffect.transform.rotation = Quaternion.LookRotation(hitNormal);
+            hitEffect.Play();
+
+            //피격 효과음 재생
+            enemyAudioPlayer.PlayOneShot(hitSound);
+        }
+
+        // LivingEntity의 OnDamage()를 실행하여 데미지 적용
+        base.OnDamage(damage, hitPoint, hitNormal);
+    }
+
+    // 사망 처리
+    public override void Die()
+    {
+        // LivingEntity의 Die()를 실행하여 기본 사망 처리 실행
+        base.Die();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행   
+    }
+}
